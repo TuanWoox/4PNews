ff626fa788b62bf650bd0b4a41a8c630a806af15
TuanWoox <z1venttt@gmail.com>
2024-12-20 16:08:27 +0700

    Finish Account Function

---
+++ passportAuth/passportGithub.js
@@ -0,0 +1,37 @@
+const GitHubStrategy = require('passport-github2').Strategy;
+const User = require('../models/user');
+module.exports = new GitHubStrategy(
+  {
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: '/account/signIn/githubAuth/callback',
+    scope: ['user:email']
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    try {
+      // Check if a user with this GitHub ID already exists
+      let user = await User.findOne({ githubId: profile.id });
+      if (!user) {
+        // Check if a user with the same email already exists
+        user = await User.findOne({ email: profile.emails[0].value });
+        if (user) {
+          // If a user exists, link their GitHub ID and save
+          user.githubId = profile.id;
+          await user.save();
+        } else {
+          // If not, create a new user
+          user = await User.create({
+            githubId: profile.id,
+            email: profile.emails[0].value,
+            fullName: profile.displayName || profile.username,
+          });
+        }
+      }
+
+      // Pass the user to Passport
+      done(null, user);
+    } catch (error) {
+      done(error);
+    }
+  }
+);
